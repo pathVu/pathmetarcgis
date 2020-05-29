b154ca94acca7b9c7265faf07d89efedcc22fb6c
Chuck Rakaczky <cjr105@case.edu>
2020-05-29 10:11:40 -0400

    delete everything

--- firmware/source/vcom.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-class VCOM {
-public:
-    void init();
-    void update();
-};
-
--- pathmet/flush.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef FLUSH_HPP
-#define FLUSH_HPP
-
-#include <boost/asio.hpp>
-
-boost::system::error_code flush(boost::asio::serial_port& serial_port);
-
-#endif
