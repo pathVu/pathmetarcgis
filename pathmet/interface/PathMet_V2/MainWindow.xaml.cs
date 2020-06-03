12393d317abf6ebc4b74d8089d4e03b648158743
Chuck Rakaczky <cjr105@case.edu>
2020-06-03 14:17:53 -0400

    backup before implementing path geometry editing

--- pathmet/interface/PathMet_V2/MainWindow.xaml.cs
+++ pathmet/interface/PathMet_V2/MainWindow.xaml.cs
@@ -1,40 +1,21 @@
 ﻿
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Threading;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Management;
-using System.IO;
-using System.Net.Sockets;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
 using Esri.ArcGISRuntime.Mapping;
 using Esri.ArcGISRuntime.Portal;
 using Esri.ArcGISRuntime.UI;
-using Esri.ArcGISRuntime.Data;
 using Esri.ArcGISRuntime.Geometry;
-using Esri.ArcGISRuntime.Symbology;
-using System.Drawing;
-using Esri.ArcGISRuntime.UI.Controls;
 using Esri.ArcGISRuntime.Security;
+using Esri.ArcGISRuntime.Symbology;
+using Esri.ArcGISRuntime.UI.Controls;
 using Color = System.Drawing.Color;
-using Esri.ArcGISRuntime.Location;
-using OpenCvSharp.Aruco;
-using System.Data.SqlTypes;
-using System.Threading;
 
 namespace PathMet_V2
 {
@@ -51,7 +32,11 @@ namespace PathMet_V2
 
             InitializeUI();
 
+            //InitializeAuthentication();
+
             InitializeMap();
+
+
 
             sensors = new SerialSensors(Properties.Settings.Default.SensorsPort);
             sensors.UpdateEvent += OnUpdate;
@@ -299,9 +284,10 @@ namespace PathMet_V2
             //Map newMap = new Map(Basemap.CreateNavigationVector());
 
             //add pathvu specific map to mapView
+            
             var mapId = "8ff831ddf02344cda858a37c742804dc";
             var webMapUrl = string.Format("https://www.arcgisonline.com/sharing/rest/content/items/{0}/data", mapId);
-            Esri.ArcGISRuntime.Mapping.Map currentMap = new Esri.ArcGISRuntime.Mapping.Map(new System.Uri(webMapUrl));
+            Map currentMap = new Map(new Uri(webMapUrl));
 
             //drawing initialization
 
@@ -358,7 +344,7 @@ namespace PathMet_V2
 
             //show length of past run
             PastRunDistContainer.Visibility = Visibility.Visible;
-            restartDrawingBtn.Visibility = Visibility.Visible;
+            pathDrawingControls.Visibility = Visibility.Visible;
             lastRunDist = GetLastRunDist();
             pastRunDistTxt.Text = lastRunDist.ToString() + "m";
 
@@ -420,7 +406,7 @@ namespace PathMet_V2
             MyMapView.DismissCallout();
             UpdateUI_waitForEndPt();
             //delete this comment
-            
+
         }
 
         private void EndPt_Tapped(object sender, GeoViewInputEventArgs e)
@@ -445,13 +431,13 @@ namespace PathMet_V2
 
 
             //get the length of the line we've drawn so far
-            double lineLength = Math.Round(GeometryEngine.LengthGeodetic(polyLineBuilder.ToGeometry(), LinearUnits.Meters, GeodeticCurveType.Geodesic),1);
+            double lineLength = Math.Round(GeometryEngine.LengthGeodetic(polyLineBuilder.ToGeometry(), LinearUnits.Meters, GeodeticCurveType.Geodesic), 1);
 
             String lengthStatus;
 
             System.Windows.Media.Color bkgdColor;
 
-            if(Math.Abs(lineLength - GetLastRunDist()) <= RunDistTolerance)
+            if (Math.Abs(lineLength - GetLastRunDist()) <= RunDistTolerance)
             {
                 endPtChosen = true;
                 lengthStatus = "Drawn length matches last run data.";
@@ -479,12 +465,6 @@ namespace PathMet_V2
 
         }
 
-        void ShowCallout(MapPoint point)
-        {
-            CalloutDefinition calloutDef = new CalloutDefinition("Start");
-            MyMapView.ShowCalloutAt(point, calloutDef);
-        }
-
         private void StopRegisteringMapTaps()
         {
             MyMapView.GeoViewTapped -= StartPt_Tapped;
@@ -509,7 +489,7 @@ namespace PathMet_V2
             btnBrokenSidewalk.IsEnabled = false;
             btnOther.IsEnabled = false;
             PastRunDistContainer.Visibility = Visibility.Hidden;
-            restartDrawingBtn.Visibility = Visibility.Hidden;
+            pathDrawingControls.Visibility = Visibility.Hidden;
             PostRunControlsPanel.Visibility = Visibility.Hidden;
             RunControlsPanel.Visibility = Visibility.Visible;
 
@@ -634,37 +614,283 @@ namespace PathMet_V2
 
         #region User_Functions
 
-        ArcGISPortal portal;
-        PortalUser user;
+        // Constants for OAuth-related values.
+        // - The URL of the portal to authenticate with
+        private const string ServerUrl = "https://chuckr.maps.arcgis.com/home/";
+        // - The Client ID for an app registered with the server (the ID below is for a public app created by the ArcGIS Runtime team).
+        private const string AppClientId = @"1EkwvPiVbGnxcwq1";
+        // - An optional client secret for the app (only needed for the OAuthAuthorizationCode authorization type).
+        private const string ClientSecret = "6e69276bfa7645e7a5718f0c2d1da805";
+        // - A URL for redirecting after a successful authorization (this must be a URL configured with the app).
+        private const string OAuthRedirectUrl = @"my-ags-app://auth";
+        // - The ID for a web map item hosted on the server (the ID below is for a traffic map of Paris).
+        private const string WebMapId = "10ebabd2fa134ebfb1e7664b4a744160";
 
-        private void loginBtn_Click(object sender, RoutedEventArgs e)
+
+        private async void InitializeAuthentication()
         {
-            Login L = new Login();
-            L.ShowDialog();
+            try
+            {
+                // Set up the AuthenticationManager to use OAuth for secure ArcGIS Online requests.
+                SetOAuthInfo();
+
+                // Connect to the portal (ArcGIS Online, for example).
+                ArcGISPortal arcgisPortal = await ArcGISPortal.CreateAsync(new Uri(ServerUrl));
+
+
+                // Get a web map portal item using its ID.
+                // If the item contains layers not shared publicly, the user will be challenged for credentials at this point.
+                PortalItem portalItem = await PortalItem.CreateAsync(arcgisPortal, WebMapId);
+
+                // Create a new map with the portal item and display it in the map view.
+                // If authentication fails, only the public layers are displayed.
+                Map myMap = new Map(portalItem);
+                MyMapView.Map = myMap;
+            }
+            catch (Exception e)
+            {
+                System.Windows.MessageBox.Show(e.ToString(), "Error starting sample");
+            }
         }
 
-
-        private async void LogIn(String userName, String password)
+        private void SetOAuthInfo()
         {
-            // generate an ArcGISTokenCredential using input from the user (username and password)
-            var cred = await AuthenticationManager.Current.GenerateCredentialAsync(
-                                                            new Uri("http://anorganization.maps.arcgis.com/sharing/rest"),
-                                                            userName,
-                                                            password) as ArcGISTokenCredential;
+            // Register the server information with the AuthenticationManager, including the OAuth settings.
+            ServerInfo serverInfo = new ServerInfo
+            {
+                ServerUri = new Uri(ServerUrl),
+                TokenAuthenticationType = TokenAuthenticationType.OAuthImplicit,
+                OAuthClientInfo = new OAuthClientInfo
+                {
+                    ClientId = AppClientId,
+                    RedirectUri = new Uri(OAuthRedirectUrl)
+                }
+            };
 
-            // connect to the portal, pass in the token 
-            portal = await ArcGISPortal.CreateAsync(
-                                                            new Uri("http://anorganization.maps.arcgis.com/sharing/rest"),
-                                                            cred,
-                                                            CancellationToken.None);
+            // If a client secret has been configured, set the authentication type to OAuthAuthorizationCode.
+            if (!String.IsNullOrEmpty(ClientSecret))
+            {
+                // Use OAuthAuthorizationCode if you need a refresh token (and have specified a valid client secret).
+                serverInfo.TokenAuthenticationType = TokenAuthenticationType.OAuthAuthorizationCode;
+                serverInfo.OAuthClientInfo.ClientSecret = ClientSecret;
+            }
 
-            // get the current portal user and check privileges
-            user = portal.User;
-            IEnumerable<PortalPrivilege> privileges = user.Privileges;
+            // Register this server with AuthenticationManager.
+            AuthenticationManager.Current.RegisterServer(serverInfo);
+
+            // Use the custom OAuthAuthorize class (defined in this module) to handle OAuth communication.
+            AuthenticationManager.Current.OAuthAuthorizeHandler = new OAuthAuthorize();
+
+            // Use a function in this class to challenge for credentials.
+            AuthenticationManager.Current.ChallengeHandler = new ChallengeHandler(CreateCredentialAsync);
         }
 
-        #endregion
+        public async Task<Credential> CreateCredentialAsync(CredentialRequestInfo info)
+        {
+            // ChallengeHandler function for AuthenticationManager that will be called whenever a secured resource is accessed.
+            Credential credential = null;
 
-        
+            try
+            {
+                // AuthenticationManager will handle challenging the user for credentials.
+                credential = await AuthenticationManager.Current.GenerateCredentialAsync(info.ServiceUri);
+            }
+            catch (Exception)
+            {
+                // Exception will be reported in calling function.
+                throw;
+            }
+
+            return credential;
+        }
+    }
+
+    // In a desktop (WPF) app, an IOAuthAuthorizeHandler component is used to handle some of the OAuth details. Specifically, it
+    //     implements AuthorizeAsync to show the login UI (generated by the server that hosts secure content) in a web control.
+    //     When the user logs in successfully, cancels the login, or closes the window without continuing, the IOAuthAuthorizeHandler
+    //     is responsible for obtaining the authorization from the server or raising an OperationCanceledException.
+    // Note: a custom IOAuthAuthorizeHandler component is not necessary when using OAuth in an ArcGIS Runtime Universal Windows app.
+    //     The UWP AuthenticationManager uses a built-in IOAuthAuthorizeHandler that is based on WebAuthenticationBroker.
+    public class OAuthAuthorize : IOAuthAuthorizeHandler
+    {
+        // A window to contain the OAuth UI.
+        private Window _authWindow;
+
+        // A TaskCompletionSource to track the completion of the authorization.
+        private TaskCompletionSource<IDictionary<string, string>> _taskCompletionSource;
+
+        // URL for the authorization callback result (the redirect URI configured for the application).
+        private string _callbackUrl;
+
+        // URL that handles the OAuth request.
+        private string _authorizeUrl;
+
+        // A function to handle authorization requests. It takes the URIs for the secured service, the authorization endpoint, and the redirect URI.
+        public Task<IDictionary<string, string>> AuthorizeAsync(Uri serviceUri, Uri authorizeUri, Uri callbackUri)
+        {
+            // If the TaskCompletionSource.Task has not completed, authorization is in progress.
+            if (_taskCompletionSource != null || _authWindow != null)
+            {
+                // Allow only one authorization process at a time.
+                throw new Exception("Authorization is in progress");
+            }
+
+            // Store the authorization and redirect URLs.
+            _authorizeUrl = authorizeUri.AbsoluteUri;
+            _callbackUrl = callbackUri.AbsoluteUri;
+
+            // Create a task completion source to track completion.
+            _taskCompletionSource = new TaskCompletionSource<IDictionary<string, string>>();
+
+            // Call a function to show the login controls, make sure it runs on the UI thread.
+            Dispatcher dispatcher = System.Windows.Application.Current.Dispatcher;
+            if (dispatcher == null || dispatcher.CheckAccess())
+                AuthorizeOnUIThread(_authorizeUrl);
+            else
+            {
+                Action authorizeOnUIAction = () => AuthorizeOnUIThread(_authorizeUrl);
+                dispatcher.BeginInvoke(authorizeOnUIAction);
+            }
+
+            // Return the task associated with the TaskCompletionSource.
+            return _taskCompletionSource.Task;
+        }
+
+        // A function to challenge for OAuth credentials on the UI thread.
+        private void AuthorizeOnUIThread(string authorizeUri)
+        {
+            // Create a WebBrowser control to display the authorize page.
+            System.Windows.Controls.WebBrowser authBrowser = new System.Windows.Controls.WebBrowser();
+
+            // Handle the navigating event for the browser to check for a response sent to the redirect URL.
+            authBrowser.Navigating += WebBrowserOnNavigating;
+
+            // Display the web browser in a new window.
+            _authWindow = new Window
+            {
+                Content = authBrowser,
+                Height = 420,
+                Width = 350,
+                WindowStartupLocation = WindowStartupLocation.CenterOwner
+            };
+
+            // Set the app's window as the owner of the browser window (if main window closes, so will the browser).
+            if (System.Windows.Application.Current != null && System.Windows.Application.Current.MainWindow != null)
+            {
+                _authWindow.Owner = System.Windows.Application.Current.MainWindow;
+            }
+
+            // Handle the window closed event then navigate to the authorize url.
+            _authWindow.Closed += OnWindowClosed;
+            authBrowser.Navigate(authorizeUri);
+
+            // Display the Window.
+            if (_authWindow != null)
+            {
+                _authWindow.ShowDialog();
+            }
+        }
+
+        private void OnWindowClosed(object sender, EventArgs e)
+        {
+            // If the browser window closes, return the focus to the main window.
+            if (_authWindow != null && _authWindow.Owner != null)
+            {
+                _authWindow.Owner.Focus();
+            }
+
+            // If the task wasn't completed, the user must have closed the window without logging in.
+            if (_taskCompletionSource != null && !_taskCompletionSource.Task.IsCompleted)
+            {
+                // Set the task completion to indicate a canceled operation.
+                _taskCompletionSource.TrySetCanceled();
+            }
+
+            _taskCompletionSource = null;
+            _authWindow = null;
+        }
+
+        // Handle browser navigation (page content changing).
+        private void WebBrowserOnNavigating(object sender, NavigatingCancelEventArgs e)
+        {
+        // Check for a response to the callback url.
+        System.Windows.Controls.WebBrowser webBrowser = sender as System.Windows.Controls.WebBrowser;
+            Uri uri = e.Uri;
+
+            // If no browser, uri, or an empty url return.
+            if (webBrowser == null || uri == null || _taskCompletionSource == null || String.IsNullOrEmpty(uri.AbsoluteUri))
+            {
+                return;
+            }
+
+            // Check if the new content is from the callback url.
+            bool isRedirected = uri.AbsoluteUri.StartsWith(_callbackUrl);
+
+            if (isRedirected)
+            {
+                // Cancel the event to prevent it from being handled elsewhere.
+                e.Cancel = true;
+
+                // Get a local copy of the task completion source.
+                TaskCompletionSource<IDictionary<string, string>> tcs = _taskCompletionSource;
+                _taskCompletionSource = null;
+
+                // Close the window.
+                if (_authWindow != null)
+                {
+                    _authWindow.Close();
+                }
+
+                // Call a helper function to decode the response parameters (which includes the authorization key).
+                IDictionary<string, string> authResponse = DecodeParameters(uri);
+
+                // Set the result for the task completion source.
+                tcs.SetResult(authResponse);
+            }
+        }
+
+        // A helper function that decodes values from a querystring into a dictionary of keys and values.
+        private static IDictionary<string, string> DecodeParameters(Uri uri)
+        {
+            // Create a dictionary of key value pairs returned in an OAuth authorization response URI query string.
+            string answer = "";
+
+            // Get the values from the URI fragment or query string.
+            if (!String.IsNullOrEmpty(uri.Fragment))
+            {
+                answer = uri.Fragment.Substring(1);
+            }
+            else
+            {
+                if (!String.IsNullOrEmpty(uri.Query))
+                {
+                    answer = uri.Query.Substring(1);
+                }
+            }
+
+            // Parse parameters into key / value pairs.
+            Dictionary<string, string> keyValueDictionary = new Dictionary<string, string>();
+            string[] keysAndValues = answer.Split(new[] { '&' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string kvString in keysAndValues)
+            {
+                string[] pair = kvString.Split('=');
+                string key = pair[0];
+                string value = string.Empty;
+                if (key.Length > 1)
+                {
+                    value = Uri.UnescapeDataString(pair[1]);
+                }
+
+                keyValueDictionary.Add(key, value);
+            }
+
+            // Return the dictionary of string keys/values.
+            return keyValueDictionary;
+
+
+            #endregion
+
+
+        }
     }
 }
